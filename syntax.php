e77cce115366a51544880841a8683b631c995dab
Andreas Gohr <gohr@cosmocode.de>
2011-05-31 11:57:59 +0200

    first working version of scraping

--- syntax.php
+++ syntax.php
@@ -8,47 +8,122 @@
 
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
-
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
 require_once DOKU_PLUGIN.'syntax.php';
 
+define('HTMLPURIFIER_PREFIX', dirname(__FILE__));
+require_once HTMLPURIFIER_PREFIX.'/HTMLPurifier.standalone.php';
+require_once HTMLPURIFIER_PREFIX.'/phpQuery-onefile.php';
+
+
+
 class syntax_plugin_scrape extends DokuWiki_Syntax_Plugin {
     public function getType() {
-        return 'FIXME: container|baseonly|formatting|substition|protected|disabled|paragraphs';
+        return 'substition';
     }
 
     public function getPType() {
-        return 'FIXME: normal|block|stack';
+        return 'block';
     }
 
     public function getSort() {
-        return FIXME;
+        return 301;
     }
 
 
     public function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('<FIXME>',$mode,'plugin_scrape');
-//        $this->Lexer->addEntryPattern('<FIXME>',$mode,'plugin_scrape');
+        $this->Lexer->addSpecialPattern('{{scrape>.+?}}',$mode,'plugin_scrape');
     }
 
-//    public function postConnect() {
-//        $this->Lexer->addExitPattern('</FIXME>','plugin_scrape');
-//    }
 
     public function handle($match, $state, $pos, &$handler){
-        $data = array();
+        $match = substr($match, 9, -2);
+        list($url, $title) = explode('|', $match, 2);
+        list($url, $query) = explode(' ', $url, 2);
+        //FIXME handle refresh parameter
+        list($url, $hash)  = explode('#', $url, 2);
+        if($hash)   $query = trim('#'.$hash.' '.$query);
+        if(!$query) $query = 'body';
+
+        //FIXME support interwiki shortcuts
+
+        $data = array(
+            'url'   => $url,
+            'title' => $title,
+            'query' => $query,
+        );
 
         return $data;
     }
 
-    public function render($mode, &$renderer, $data) {
+    public function render($mode, &$R, $data) {
         if($mode != 'xhtml') return false;
+
+        // fetch remote data
+        $http = new DokuHTTPClient();
+        $resp = $http->get($data['url']);
+
+        if(!$resp){
+            $R->doc .= 'Failed to load remote ressource';
+            return true;
+        }
+
+        // determine mime type
+        list($mime,$charset) = explode(';',$http->resp_headers['content-type']);
+        $mime    = trim(strtolower($mime));
+        $charset = trim(strtolower($charset));
+        $charset = preg_replace('/charset *= */','',$charset);
+
+
+        if(preg_match('/image\/(gif|png|jpe?g)/',$mime)){
+            // image embed
+            $R->externalmedia ($data['url'], $data['title']);
+        }elseif(preg_match('/text\//',$mime)){
+            if($charset != 'utf-8'){
+                $resp = utf8_encode($resp); // we just assume it to be latin1
+            }
+
+            if(preg_match('/text\/html/',$mime)){
+                // display HTML
+                $this->display_html($data,$resp,$R);
+
+                //FIXME support directory listings?
+            }else{
+                // display as code
+                $R->preformatted($resp);
+            }
+        }else{
+            $R->doc .= 'Failed to handle mime type '.hsc($mime);
+            return true;
+        }
 
         return true;
     }
+
+    private function display_html($data,$resp,&$R){
+        // extract the wanted part from the HTML using the given query
+        phpQuery::newDocument($resp);
+        $pq = pq($data['query']);
+
+        // get all wanted HTML by converting the DOMElements back to HTML
+        $html = '';
+        foreach($pq->elements as $elem){
+            $html .= $elem->ownerDocument->saveXML($elem);
+        }
+
+        // clean up HTML
+        $purifier = new HTMLPurifier();
+        $purifier->config->set('Attr.EnableID', true);
+        $purifier->config->set('Attr.IDPrefix', 'scrape___');
+        $purifier->config->set('URI.Base', $data['url']);
+        $purifier->config->set('URI.MakeAbsolute', true);
+        $html = $purifier->purify($html);
+
+        $R->doc .= $html;
+    }
+
+
 }
 
 // vim:ts=4:sw=4:et:
